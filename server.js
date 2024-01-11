dc141a000eef549875dde19cfb77d5b3d30f102c
testliveaz <155069408+testliveaz@users.noreply.github.com>
2024-01-12 02:42:53 +0400

    Update server.js4

--- server.js
+++ server.js
@@ -115,6 +115,6 @@ app.post('/upsert-count', async (req, res) => {
 app.use(express.static('public'));
 
 // Start http listener
-const port = process.env.PORT || 7784;
+const port = process.env.PORT || 8584;
 httpServer.listen(port);
 console.info(`Server running! Please visit http://localhost:${port}`);
